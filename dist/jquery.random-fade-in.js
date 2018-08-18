d0a5c7e45d98db688da4717861a1317f2f089052
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-08-18 10:29:33 +0900

    Write comments in English Improve _shuffleOrder() method

--- dist/jquery.random-fade-in.js
+++ dist/jquery.random-fade-in.js
@@ -17,7 +17,7 @@
 /**
  * @function external:"jQuery.fn".randomFadeIn
  * @arg {null|string|number} [duration='slow'] - Duration. Same to jQuery ".fadeIn()"
- * @arg {boolean}            [isLoop=true]     - Whether to repeat.
+ * @arg {boolean} [isLoop=true] - Whether to repeat
  * @return {Object} jQuery object
  */
 $.fn.randomFadeIn = function (duration, isLoop) {
@@ -28,80 +28,77 @@ $.fn.randomFadeIn = function (duration, isLoop) {
 
 /**
  * @class external:jQuery.randomFadeIn
- * @classdesc 要素ごとに適用される処理を集めたクラス
- * @arg {Object} elem - プラグインを適用するHTML要素
- * @arg {null|string|number} [duration='slow'] - フェードインにかかる時間 (jQuery.fadeIn()の引数と同じ形式)
- * @arg {boolean} [isLoop=true] - フェードインと消去を繰り返すかどうかの真偽値
- * @prop {Object} elem - プラグインを適用するHTML要素
- * @prop {} duration - フェードインにかかる時間 (jQuery.fadeIn()の引数と同じ形式)
- * @prop {boolean} isLoop - フェードインと消去を繰り返すかどうかの真偽値
- * @prop {number} len - フェードインする要素の数
- * @prop {Array} order - フェードインする順番
+ * @arg {Object} elem - An element to apply this plugin
+ * @arg {null|string|number} duration - Duration. Same to jQuery ".fadeIn()"
+ * @arg {boolean} isLoop - Whether to repeat
+ * @prop {Object} elemChildren - Children of an element to apply this plugin
+ * @prop {string|number} duration - Duration. Same to jQuery ".fadeIn()"
+ * @prop {boolean} isLoop - Whether to repeat
+ * @prop {Array} order - An array that contains indexes of elements to be shuffled
  */
 $.randomFadeIn = function(elem, duration, isLoop) {
   this.elemChildren = $(elem).children();
   $(this.elemChildren).children().stop(true, false).hide();
-  this.duration = (!duration) ? 'slow' : duration;
+  this.duration = duration || 'slow';
   this.isLoop = (isLoop === undefined) ? true : isLoop;
 
-  //要素の数だけ配列を作成
-  this.len = $(this.elemChildren).length;
+  // Create an array that contains order
   this.order = [];
-  for (var i = 0; i < this.len; i++) {
+  for (var i = 0, len = $(this.elemChildren).length; i < len; i++) {
     this.order[i] = i;
   }
 
-  this.order = this._shuffleOrder(this.len, this.order);
+  this.order = this._shuffleOrder(this.order);
   this._fadeInEach(0);
 };
 
 $.extend($.randomFadeIn.prototype, /** @lends external:jQuery.randomFadeIn.prototype */ {
-
   /**
+   * Shuffle the array of elements
+   * 
    * @private
-   * @desc Shuffle the array of elements
-   * @arg {number} len   - The number of elements to be shuffled
    * @arg {Array}  order - An array that contains indexes of elements to be shuffled
    * @return {Array} An shuffled array
    */
-  _shuffleOrder: function(len, order) {
-    var idxLast = len;
+  _shuffleOrder: function(order) {
+    var idxLast = order.length;
     while (idxLast > 0) {
       var idxRandom = Math.floor(Math.random() * idxLast);
       idxLast--;
       var elemLast = order[idxLast];
 
-      // ランダムで選んだものと最後尾とを交換する
-      order[idxLast] = order[idxRandom];
+      // Exchange each other
+      order[idxLast]   = order[idxRandom];
       order[idxRandom] = elemLast;
     }
     return order;
   },
 
   /**
+   * Run fade-in
+   * 
    * @private
-   * @desc Run fade-in
    * @arg {number} idx - An index of element to be fade-in
    */
   _fadeInEach: function(idx) {
+    var len = $(this.elemChildren).length;
     var self = this;
     $(this.elemChildren).eq(this.order[idx]).children().fadeIn(
-      self.duration,
+      this.duration,
       function() {
         idx++;
-        if (idx < self.len) {
-          //すべての要素を表示させるまで、もう一度同じ関数を実行する。
+        if (idx < len) {
+          // Run recursively until all elements fade in
           self._fadeInEach(idx);
         } else if (self.isLoop) {
-          //ループの指示があれば、繰り返す。
+          // Repeat fadeIn
           $(self.elemChildren).children().fadeOut(self.duration);
-          self.order = self._shuffleOrder(self.len, self.order);
+          self.order = self._shuffleOrder(self.order);
           self._fadeInEach(0);
         }
       }
     );
   }
-
 }); // end of $.extend
 
 }));
--- test/unit/jQuery.randomFadeIn/shuffleOrder.js
+++ test/unit/jQuery.randomFadeIn/shuffleOrder.js
@@ -4,8 +4,8 @@
 describe('$.randomFadeIn._shuffleOrder', () => {
   it('should return array', () => {
     let arr = [0, 1, 2, 3, 4];
-    let len = 5;
-    let res = $.randomFadeIn.prototype._shuffleOrder(len, arr);
+    let res = $.randomFadeIn.prototype._shuffleOrder(arr);
+    assert.equal(typeof res, 'object');
     assert.equal(res.length, arr.length);
   });
 });
